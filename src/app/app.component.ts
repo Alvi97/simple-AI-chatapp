3fb67b780030bbc0ff80cd60a15cb6a5d5cd7bc6
Alvi Lika <alvi.lika@moveengineering-eu.com>
2021-03-08 01:02:15 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'chatbot';
+  
+  dataset:any;
+  filterName: string = "";
+  input: any;
+  cnt =0;
+  constructor(private http: HttpClient) { }
+  values = Array<string>();
+  ngOnInit(){
+    this.http.get("../assets/intents.json").subscribe((data:any) => {
+      console.log(data);
+      this.dataset = data;
+  })
+  }
+
+  addInput(event: any){
+    
+    console.log(this.dataset.intents)
+    console.log(event.target.value);
+    let pre = this.values.length;
+    this.dataset.intents.forEach((element:any) => {
+      element.patterns.forEach((el:any) => {
+        if(event.target.value.toLowerCase().includes(el.toLowerCase())){
+          console.log(el.toLowerCase() , "?" , element.responses[Math.floor(Math.random()*element.responses.length)]);
+          this.values.push(element.responses[Math.floor(Math.random()*element.responses.length)]);
+          
+        }else if (el.toLowerCase().includes(event.target.value.toLowerCase())){
+          this.values.push(element.responses[Math.floor(Math.random()*element.responses.length)]);
+         
+        }
+
+      });
+    });
+    if(pre == this.values.length){
+      this.values.push("Sorry, Alvi didnt teach me this")
+    }
+    event.target.value = ""
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
